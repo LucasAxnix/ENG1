5a178a49fa7f95eec3f552fd139b9e8fb67dd2a3
mafflife <73513717+mafflife@users.noreply.github.com>
2020-11-02 13:39:15 +0000

    Add files via upload

---
+++ InputManager.java
@@ -0,0 +1,31 @@
+var InputManager = inherits(function() { }, {
+	'Keys': {
+		'UP' : 38
+		'DOWN' : 40
+		'LEFT' : 37
+		'RIGHT' : 39
+		'W' : 82
+		'A' : 65
+		'S' : 82
+		'D' :68
+	},
+	
+	'_keysDown': {},
+	
+	'constructor': function() {
+		window.addEventListener('keydown', _.bind(this._onkeydown, this));
+		window.addEventListener('keyup', _.bind(this._onkeyup, this));
+	},
+	
+	'_onkeydown': function(ev) {
+		this._keysDown[ev.which] = true;
+	},
+	
+	'_onkeyup': function(ev) {
+		this._keysDown[ev.which] = false;
+	},
+	
+	'keyDown': function(key) {
+		return !!this._keysDown[key];
+	}
+});

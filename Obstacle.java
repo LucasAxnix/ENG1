9c9e40e92fb44c0e4a616c0a9ad2501a1495863d
LucasAxnix <72559042+LucasAxnix@users.noreply.github.com>
2020-11-14 16:42:06 +0000

    Added a lot of game features
    Added collisions
    Added scrolling background
    Added boat selection

---
+++ Obstacle.java
@@ -0,0 +1,21 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Obstacle extends GameEntity {
+
+    int obstaclePositionX;
+
+    public Obstacle(int x, int y, BufferedImage sprite) {
+        super(0, y, sprite);
+        obstaclePositionX = x;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.drawImage(sprite, x, y, null);
+    }
+
+    @Override
+    public void update() {
+    }
+}

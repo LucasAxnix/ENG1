bc49865f2e404af6a090b5cd171b40e02b2da13a
Jalen-Chen <57817787+Jalen-Chen@users.noreply.github.com>
2020-11-19 18:59:19 +0800

    Add files via upload
    1. Add a new way to control player's boat 
    2. The faster player press space, the faster the boat will go
    3. Enable Boat health when hit a duck or rock
    4. Enable live boat health display while playing 
    5. enabled manoeuvrability when boat goes up and down

--- Boat.java
+++ Boat.java
@@ -26,7 +26,7 @@ public class Boat extends GameEntity {
 		this.durability = durability;
 		this.maneuverability = maneuverability;
 		this.name = name;
-		boatHealth = 100;
+		boatHealth = 100; // a reansonable number
 		timer = 0;
 	}
 
@@ -44,6 +44,7 @@ public class Boat extends GameEntity {
 		finishedRace = false;
 		timer = 0;
 		penalty = 0;
+		speed = 0;
 	}
 
 	public void increaseFatigue(int raceNumber) {
@@ -66,6 +67,7 @@ public class Boat extends GameEntity {
 
 	public void setOpponentBoat(int raceLane) {
 		finishedRace = false;
+		this.isPlayer = false;
 		RaceState rs = (RaceState) GameStateManager.getInstance().getState(GameStateManager.RACESTATE);
 		river = rs.getRiver();
 		x = 50;
@@ -91,11 +93,12 @@ public class Boat extends GameEntity {
 	@Override
 	public void update() {
 		increaseTimer();
-		speed += acceleration * 0.05f;
-		speed = Math.min(speed, maxSpeed);
 		if (!isPlayer) {
+			speed += acceleration *0.05f;
+			speed = Math.min(speed, maxSpeed);
 			x += speed - river.getSpeed();
 		} else {
+			speed += InputManager.getInstance().get_new_speed()*0.03f;
 			if (y + sprite.getHeight() > Game.WINDOW_HEIGHT || y < 0) {
 				GameStateManager.getInstance().setState(GameStateManager.GAMEOVERSTATE);
 			}
@@ -115,10 +118,10 @@ public class Boat extends GameEntity {
 				}
 			}
 			if (InputManager.getInstance().getUp()) {
-				y -= 1;
+				y -= maneuverability;
 			}
 			if (InputManager.getInstance().getDown()) {
-				y += 1;
+				y += maneuverability;
 			}
 		}
 	}
@@ -134,7 +137,7 @@ public class Boat extends GameEntity {
 		finishedRace = true;
 		this.racePos = racePos;
 	}
-
+//
 	public boolean getFinished() {
 		return finishedRace;
 	}
--- InputManager.java
+++ InputManager.java
@@ -1,12 +1,14 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+
 public class InputManager implements KeyListener {
 
     private static boolean up;
     private static boolean down;
     private static boolean left;
     private static boolean right;
+    private static int speed_up = 0;
 
     private static InputManager instance;
 
@@ -34,6 +36,8 @@ public class InputManager implements KeyListener {
             down = true;
         } else if (e.getKeyCode() == KeyEvent.VK_D) {
             right = true;
+        }else if (e.getKeyCode() == KeyEvent.VK_SPACE){
+            speed_up =10;
         }
     }
 
@@ -47,6 +51,8 @@ public class InputManager implements KeyListener {
             down = false;
         } else if (e.getKeyCode() == KeyEvent.VK_D) {
             right = false;
+        }else if (e.getKeyCode() == KeyEvent.VK_SPACE){
+            speed_up =0;
         }
     }
 
@@ -65,6 +71,10 @@ public class InputManager implements KeyListener {
     public boolean getRight() {
         return right;
     }
+
+    public int get_new_speed(){
+        return speed_up;
+    }// return the new speed_up given by the user.
 
     @Override
     public void keyTyped(KeyEvent e) {}
